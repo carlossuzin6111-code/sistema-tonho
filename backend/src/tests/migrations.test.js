becea29b5b760cbe6d2bef1548ca784ad7220203
Diogodesk <diogocrespi@alunos.utfpr.edu.br>
2026-07-29 13:50:01 -0300

    test: include periodization migration in order

--- backend/src/tests/migrations.test.js
+++ backend/src/tests/migrations.test.js
@@ -46,6 +46,7 @@ const migrations = [
 
   '202607290010_add_session_activity.js',
   '202607290011_create_idempotency_keys.js',
+  '202607290012_create_workout_microcycles.js',
   '202607290013_add_catalog_governance.js'
 ];
 
